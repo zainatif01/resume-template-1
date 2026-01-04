83f648829de446f79c47471ef2fc9204c05a27cb
zainatif01 <zainatif15403@gmail.com>
2026-01-04 12:00:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function loadResumeData() {
     const resumeContainer = document.getElementById('resume-content');
 
     try {
-        const response = await fetch('resume-data.json');
+        const response = await fetch('resume-data.json?v=&{Date.now()}');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
